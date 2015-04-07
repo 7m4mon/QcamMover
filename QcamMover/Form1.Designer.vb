0b10fc1f9812a2e9febb2fa4e7ba24d39ea4879c
7m4mon <7m4mon@gmail.com>
2015-04-07 22:52:58 +0900

    First Commit

---
+++ QcamMover/Form1.Designer.vb
@@ -0,0 +1,257 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナで必要です。
+    'Windows フォーム デザイナを使用して変更できます。  
+    'コード エディタを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button17 = New System.Windows.Forms.Button
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.NumericUpDown3 = New System.Windows.Forms.NumericUpDown
+        Me.NumericUpDown4 = New System.Windows.Forms.NumericUpDown
+        Me.Button15 = New System.Windows.Forms.Button
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Button16 = New System.Windows.Forms.Button
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown
+        Me.Button14 = New System.Windows.Forms.Button
+        Me.Button13 = New System.Windows.Forms.Button
+        Me.Button12 = New System.Windows.Forms.Button
+        Me.Button11 = New System.Windows.Forms.Button
+        Me.GroupBox3.SuspendLayout()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Button1)
+        Me.GroupBox3.Controls.Add(Me.Button17)
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Controls.Add(Me.NumericUpDown3)
+        Me.GroupBox3.Controls.Add(Me.NumericUpDown4)
+        Me.GroupBox3.Controls.Add(Me.Button15)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Controls.Add(Me.Button16)
+        Me.GroupBox3.Controls.Add(Me.NumericUpDown2)
+        Me.GroupBox3.Controls.Add(Me.NumericUpDown1)
+        Me.GroupBox3.Controls.Add(Me.Button14)
+        Me.GroupBox3.Controls.Add(Me.Button13)
+        Me.GroupBox3.Controls.Add(Me.Button12)
+        Me.GroupBox3.Controls.Add(Me.Button11)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(445, 81)
+        Me.GroupBox3.TabIndex = 21
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "QCAM"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(375, 47)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(50, 23)
+        Me.Button1.TabIndex = 15
+        Me.Button1.Text = "About"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(305, 47)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(64, 23)
+        Me.Button17.TabIndex = 14
+        Me.Button17.Text = "AMove"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(229, 52)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(20, 12)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "AY"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(148, 52)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(20, 12)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "AX"
+        '
+        'NumericUpDown3
+        '
+        Me.NumericUpDown3.Location = New System.Drawing.Point(174, 50)
+        Me.NumericUpDown3.Maximum = New Decimal(New Integer() {64, 0, 0, 0})
+        Me.NumericUpDown3.Minimum = New Decimal(New Integer() {64, 0, 0, -2147483648})
+        Me.NumericUpDown3.Name = "NumericUpDown3"
+        Me.NumericUpDown3.Size = New System.Drawing.Size(44, 19)
+        Me.NumericUpDown3.TabIndex = 11
+        '
+        'NumericUpDown4
+        '
+        Me.NumericUpDown4.Location = New System.Drawing.Point(255, 49)
+        Me.NumericUpDown4.Maximum = New Decimal(New Integer() {27, 0, 0, 0})
+        Me.NumericUpDown4.Minimum = New Decimal(New Integer() {27, 0, 0, -2147483648})
+        Me.NumericUpDown4.Name = "NumericUpDown4"
+        Me.NumericUpDown4.Size = New System.Drawing.Size(44, 19)
+        Me.NumericUpDown4.TabIndex = 10
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(375, 17)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(50, 23)
+        Me.Button15.TabIndex = 4
+        Me.Button15.Text = "Reset"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(229, 23)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(20, 12)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "RY"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(148, 23)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(20, 12)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "RX"
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(305, 18)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(64, 23)
+        Me.Button16.TabIndex = 7
+        Me.Button16.Text = "RMove"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(255, 21)
+        Me.NumericUpDown2.Maximum = New Decimal(New Integer() {54, 0, 0, 0})
+        Me.NumericUpDown2.Minimum = New Decimal(New Integer() {54, 0, 0, -2147483648})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(44, 19)
+        Me.NumericUpDown2.TabIndex = 6
+        Me.NumericUpDown2.Value = New Decimal(New Integer() {5, 0, 0, 0})
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(174, 21)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {128, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {128, 0, 0, -2147483648})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(44, 19)
+        Me.NumericUpDown1.TabIndex = 5
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {10, 0, 0, 0})
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(99, 32)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(41, 23)
+        Me.Button14.TabIndex = 3
+        Me.Button14.Text = "Right"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(52, 46)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(41, 23)
+        Me.Button13.TabIndex = 2
+        Me.Button13.Text = "Down"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(52, 16)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(41, 23)
+        Me.Button12.TabIndex = 1
+        Me.Button12.Text = "Up"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(8, 32)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(38, 23)
+        Me.Button11.TabIndex = 0
+        Me.Button11.Text = "Left"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(472, 106)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "QcamMover"
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button17 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents NumericUpDown3 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown4 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Button15 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button16 As System.Windows.Forms.Button
+    Friend WithEvents NumericUpDown2 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Button14 As System.Windows.Forms.Button
+    Friend WithEvents Button13 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ QcamMover/Form1.vb
@@ -0,0 +1,78 @@
+﻿Public Class Form1
+    Dim aPan As Integer = 0
+    Dim aTilt As Integer = 0
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        'Left
+        QcamMover(8, 0)
+    End Sub
+
+    Private Sub Button12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button12.Click
+        'up
+        QcamMover(0, -6)
+    End Sub
+
+    Private Sub Button13_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button13.Click
+        'down
+        QcamMover(0, 6)
+    End Sub
+
+    Private Sub Button14_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button14.Click
+        'Right
+        QcamMover(-8, 0)
+
+    End Sub
+
+    Private Sub Button15_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button15.Click
+        'reset
+        QcamMover(180, 180)
+        System.Threading.Thread.Sleep(3000)
+        QcamMover(-64, -27)
+    End Sub
+
+    Private Sub Button16_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button16.Click
+        'move
+        QcamMover(NumericUpDown1.Value, NumericUpDown2.Value) ' 引数
+
+    End Sub
+    Private Sub Button17_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button17.Click
+        '絶対値移動
+        QcamMover(NumericUpDown3.Value - aPan, NumericUpDown4.Value - aTilt)
+    End Sub
+
+    Private Sub QcamMover(ByVal rPan As Integer, ByVal rTilt As Integer)
+        Dim psInfo As New ProcessStartInfo()
+        psInfo.FileName = "Q200_PTR.exe" ' 実行するファイル
+        '引数の指定
+        psInfo.Arguments = rPan & " " & rTilt ' 引数
+        psInfo.CreateNoWindow = True ' コンソール・ウィンドウを開かない
+        psInfo.UseShellExecute = False ' シェル機能を使用しない
+
+        Process.Start(psInfo)
+
+        aPan = aPan + rPan
+        If aPan > NumericUpDown3.Maximum Then
+            aPan = NumericUpDown3.Maximum
+        ElseIf aPan < NumericUpDown3.Minimum Then
+            aPan = NumericUpDown3.Minimum
+        Else
+            'aPan = NumericUpDown3.Value + rPan
+        End If
+        NumericUpDown3.Value = aPan
+
+        aTilt = aTilt + rTilt
+        If aTilt > NumericUpDown4.Maximum Then
+            aTilt = NumericUpDown4.Maximum
+        ElseIf aTilt < NumericUpDown4.Minimum Then
+            aTilt = NumericUpDown4.Minimum
+        Else
+            'aTilt = NumericUpDown4.Value + rTilt
+        End If
+        NumericUpDown4.Value = aTilt
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        AboutBox1.Show()
+    End Sub
+End Class
+
